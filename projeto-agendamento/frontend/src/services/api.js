97918acc94ea84110f409d062968b9e3d73dad61
Jvpdo <sxacasd@gmail.com>
2026-03-15 11:12:51 -0300

    Fix: Skip ngrok browser warning

--- projeto-agendamento/frontend/src/services/api.js
+++ projeto-agendamento/frontend/src/services/api.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://ennuyante-coinstantaneous-rosemarie.ngrok-free.dev'
-                
+    baseURL: 'https://ennuyante-coinstantaneous-rosemarie.ngrok-free.dev',
+    headers: {
+    'ngrok-skip-browser-warning': 'true'
+    }
 });
 
 export default api;
