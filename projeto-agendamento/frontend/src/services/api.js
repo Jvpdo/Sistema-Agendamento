c05a396b0f8e135778912a5b55e54f5499de0e0d
Jvpdo <sxacasd@gmail.com>
2026-03-15 13:56:39 -0300

    Deploy: Conectando frontend ao backend definitivo no Railway

--- projeto-agendamento/frontend/src/services/api.js
+++ projeto-agendamento/frontend/src/services/api.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://ennuyante-coinstantaneous-rosemarie.ngrok-free.dev',
-    headers: {
-    'ngrok-skip-browser-warning': 'true'
-    }
+    baseURL: 'sistema-agendamento-production-1690.up.railway.app',
+    
 });
 
 export default api;
